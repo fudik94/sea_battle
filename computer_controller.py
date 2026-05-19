b057e2e34ff54a7a61d0855c2eb0b7cdb6355582
Danil <cernovd241@gmail.com>
2026-05-20 00:03:50 +0300

    US-026: Add constructors to PlayerController and ComputerController for separated creation logic

--- computer_controller.py
+++ computer_controller.py
@@ -1,9 +1,16 @@
 from controller_class import ControllerClass
 
-
 class ComputerController(ControllerClass):
     EASY = "easy"
     MEDIUM = "medium"
     HARD = "hard"
 
-    difficulty = MEDIUM
+    def __init__(self, name="Computer", difficulty=MEDIUM):
+        super().__init__(name)
+        self.difficulty = difficulty
+
+    def take_turn(self, grid):
+        pass
+
+    def place_ships(self, grid):
+        pass
--- player_controller.py
+++ player_controller.py
@@ -1,5 +1,11 @@
 from controller_class import ControllerClass
 
-
 class PlayerController(ControllerClass):
-    pass
+    def __init__(self, name="Player"):
+        super().__init__(name)
+
+    def take_turn(self, grid):
+        pass
+
+    def place_ships(self, grid):
+        pass
