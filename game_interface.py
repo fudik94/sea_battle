84d9c8388051f3c59cacb245fc47745620cdaaea
Fuad Ismayilbayli <fuad094@gmail.com>
2026-05-21 16:52:28 +0300

    US-072: Add show_grid() function to display game grid in CLI

--- game_interface.py
+++ game_interface.py
@@ -4,3 +4,8 @@ class GameInterface:
 
     def ask(self, prompt: str) -> str:
         return ""
+
+    def show_grid(self, grid):
+        for row in grid.grid:
+            print(' '.join(cell.value for cell in row))
+
