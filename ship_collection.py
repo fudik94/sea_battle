2d0f0a6e5377b518749e17e37cf5985ab4e1ebc2
Fuad Ismayilbayli <fuad094@gmail.com>
2026-05-21 16:09:41 +0300

    US-061: Add ship limit to ShipCollection, default 5, configurable for tests

--- ship_collection.py
+++ ship_collection.py
@@ -1,11 +1,17 @@
 from ship_representor import ShipRepresentor
 
 class ShipCollection:
-    def __init__(self):
+    MAX_SHIPS = 5
+
+    def __init__(self, max_ships: int = MAX_SHIPS):
         self.ships: list[ShipRepresentor] = []
+        self.max_ships = max_ships
 
     def add(self, ship_representor: ShipRepresentor):
+        if len(self.ships) >= self.max_ships:
+            return False
         self.ships.append(ship_representor)
+        return True
 
     def all_sunk(self):
         return all(s.is_sunk() for s in self.ships)
